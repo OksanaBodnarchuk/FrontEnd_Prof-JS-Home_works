ae951c5010fe3685e31792858361257f39609042
Oksana Bodnarchuk <110978467+OksanaBodnarchuk@users.noreply.github.com>
2023-01-06 18:35:13 +0100

    HW_9.12.22

--- HW_9.12.22/script.js
+++ HW_9.12.22/script.js
@@ -1,12 +1,84 @@
-const textarea = document.getElementById("textarea");
+const form = document.querySelector(".form");
+const container = document.querySelector(".products");
+const totalPrice = document.querySelector(".total");
+const totalAmount = document.querySelector(".amount");
 
-// При загрузке страницы получаем текст из хранилища
-const savedText = localStorage.getItem("text");
-if (savedText) {
-  textarea.value = savedText;
+let goods = JSON.parse(localStorage.getItem("goods")) ?? [];
+
+createNewCard();
+countTotalPrice();
+countTotalAmouunt();
+
+form.addEventListener("submit", (event) => {
+  event.preventDefault();
+
+  const title = form.title.value;
+  const price = form.price.value;
+  const amount = form.amount.value;
+  const id = Date.now();
+
+  if (+price && +amount) {
+    goods.push({ id, title, price, amount });
+  }
+
+  form.title.value = "";
+  form.price.value = "";
+  form.amount.value = "";
+
+  createNewCard();
+  countTotalPrice();
+  countTotalAmouunt();
+
+  localStorage.setItem("goods", JSON.stringify(goods));
+});
+
+function createNewCard() {
+  container.innerText = "";
+  goods.forEach(({ id, title, price, amount }) => {
+    const card = createCard(id, title, price, amount);
+    container.append(card);
+  });
 }
 
-// При изменении текста в поле ввода сохраняем его в хранилище
-textarea.addEventListener("input", () => {
-  localStorage.setItem("text", textarea.value);
-});
+function createCard(id, title, price, amount) {
+  const card = document.createElement("div");
+  const cardTitle = document.createElement("p");
+  const cardPrice = document.createElement("p");
+  const cardCount = document.createElement("p");
+  const delButton = document.createElement("button");
+
+  cardTitle.className = "product_title";
+  cardPrice.className = "product_price";
+  cardCount.className = "product_count";
+  delButton.className = "delButton";
+
+  cardTitle.innerText = title;
+  cardPrice.innerText = price;
+  cardCount.innerText = `${price} x ${amount} = ${price * amount}`;
+  delButton.innerHTML = "DELETE";
+
+  delButton.addEventListener("click", (event) => delCard(id));
+
+  card.append(cardTitle, cardPrice, cardCount, delButton);
+  return card;
+}
+
+function delCard(cardId) {
+  goods = goods.filter((prod) => prod.id != cardId);
+  localStorage.setItem("goods", JSON.stringify(goods));
+  createNewCard();
+  countTotalPrice();
+  countTotalAmouunt();
+}
+
+function countTotalPrice() {
+  totalPrice.innerText = "";
+  const countPrice = goods.reduce((sum, el) => sum + +el.price * +el.amount, 0);
+  totalPrice.innerText = `Общая стоимость: ${countPrice}`;
+}
+
+function countTotalAmouunt() {
+  totalAmount.innerText = "";
+  const countAmount = goods.reduce((sum, { amount }) => sum + +amount, 0);
+  totalAmount.innerText = `Общее количество: ${countAmount}`;
+}
